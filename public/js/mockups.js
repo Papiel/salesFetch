0fafb830f5e188222cdaade226fff4ee32933249
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-06-02 12:05:56 +0200

    Left Panel on tablet

--- public/js/mockups.js
+++ public/js/mockups.js
@@ -17,21 +17,54 @@ $('#context-switch a').click(function() {
 /*===============
   TABLET
  ================*/
+if ($('body.tablet').length) {
+  /**
+   * Filtering
+   */
+  $("#filter").popover({
+    placement : 'bottom',
+    html: true,
+    container: '.navbar-result',
+    content : $('#filter-template').html()
+  });
 
-/**
- * Toogle left panel
- */
-$('#left-toogle').click(function() {
-  $("#left-panel").toggleClass('active');
-});
+  $('#filter').on('hidden.bs.popover', function () {
+    $('body').unbind('click');
+  });
 
-/**
- * Hide left bar on click snippet
- */
-$('.snippet').click(function(e) {
-  e.preventDefault();
-  $("#left-panel").removeClass('active');
-});
+  $('#filter').on('shown.bs.popover', function () {
+    $('body').bind('click', function(e) {
+      if ($(e.target).data('toggle') !== 'popover' && $(e.target).parents('.popover.in').length === 0) {
+        $('#filter').popover('hide');
+      }
+    });
+  });
+
+  $("#left-panel").on('click', '.dismiss', function(e) {
+    e.preventDefault();
+    $('#filter').popover('hide');
+  });
+
+  $("#left-panel").on('click', '.execute', function(e) {
+    e.preventDefault();
+    location.reload();
+  });
+
+  /**
+   * Toogle left panel
+   */
+  $('#left-toogle').click(function() {
+    $("#left-panel").toggleClass('active');
+  });
+
+  /**
+   * Hide left bar on click snippet
+   */
+  $('.snippet').click(function(e) {
+    e.preventDefault();
+    $("#left-panel").removeClass('active');
+  });
+}
 
 
 

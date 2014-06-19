7e1a3ca57b1a52f02337ad830716c3e09aa336fe
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-06-19 15:53:51 +0200

    findDoucments should expect 200

--- app/helpers/anyfetch.js
+++ app/helpers/anyfetch.js
@@ -53,6 +53,7 @@ module.exports.findDocuments = function(params, user, cb) {
 
       request(fetchApiUrl).get(batchUrl)
         .set('Authorization', 'Bearer ' + user.anyFetchToken)
+        .expect(200)
         .end(cb);
     },
     function templateResults(res, cb) {

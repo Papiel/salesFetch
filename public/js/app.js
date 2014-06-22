5d8dd7c6520c25eb528960bf6c375b2ac8c0b3dc
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-06-22 10:41:31 -0700

    Salesfetch module

--- public/js/app.js
+++ public/js/app.js
@@ -1,141 +1,126 @@
 'use strict';
 
-var attachedViewer = null;
-var data = window.data;
+window.salesFetchModule = (function() {
+  var _data = {};
 
-var goToLocation = function(window, url) {
-  var linker = url.indexOf('?') !== -1 ? '&' : '?';
-  var urlWithData = url + linker + "data=" + encodeURIComponent(JSON.stringify(data));
-  window.location = urlWithData;
-};
+  var _setDataInUrl = function(url) {
+    var linker = url.indexOf('?') !== -1 ? '&' : '?';
+    return url + linker + "data=" + encodeURIComponent(JSON.stringify(_data));
+  };
 
-var displayFull = function(url) {
-  if (!attachedViewer) {
-    // Create a new viewer and display the right Url
-    attachedViewer = window.open(null,"_blank","toolbar=yes, scrollbars=yes, resizable=yes, width=800, height=1000");
-    attachedViewer.document.write('loading...');
-    goToLocation(attachedViewer, url);
+  var ret = {};
 
-    var interval = window.setInterval(function() {
-        try {
-          if (attachedViewer === null || attachedViewer.closed) {
-            window.clearInterval(interval);
-            attachedViewer = null;
-          }
-        }
-        catch (e) {
-        }
-      }, 200);
-  } else {
-    attachedViewer.document.write('loading...');
-    goToLocation(attachedViewer, url);
-    attachedViewer.focus();
-  }
-};
+  /**
+   * Init the salesFetch module
+   */
+  ret.init = function() {
+    if (!window.jQuery) {
+      return new Error('jQuery hasn\'t been loaded');
+    }
 
-/**
- * Open documents URL in custom window
- */
-$(function() {
-  if ($('.document').length !== 0) {
+    if (!window.data) {
+      return new Error('Inivalid data');
+    }
 
-    var isViewer = window.opener ? true : false;
-    var isOnMobile = data.env.deviseType === "mobile";
+    _data = window.data;
+    return this;
+  };
 
-    // Handle the full preview loading
-    $(".snippet-list").on('click', '[data-document-url]', function(e) {
-      e.preventDefault();
+  /**
+   * Retrieve the list of providers
+   * @param cb(data) The callback containning the porviders
+   */
+  ret.getProviders = function(cb) {
+    var url = _setDataInUrl('/app/providers');
+    $.get(url, cb);
+  };
 
-      var url = $(this).data("documentUrl");
-      if (isOnMobile) {
-        goToLocation(window, url);
-      } else if (!isViewer) {
-        displayFull(url);
-      }
+  /**
+   * Retrieve the list snippet documents linked in the context
+   * @params {Integer} start The start index of documents
+   * @param cb(data) The callback containning the HTML generated
+   */
+  ret.getContextSearch = function(start, cb) {
+    var baseUrl = '/app/context-search';
+    if (start) {
+      baseUrl += '?start=' + start;
+    }
 
+    var url = _setDataInUrl(baseUrl);
+    $.get(url, cb);
+  };
+
+  /**
+   * Retrieve a single document
+   * @params {String} docId The id of the document to retrieve
+   */
+  ret.getDocument = function(docId, cb) {
+    if (!docId) {
+      return new Error('No document to retrieve');
+    }
+
+    var url = _setDataInUrl('/app/docment/' + docId);
+    $.get(url, cb);
+  };
+
+  /**
+   * Retrieve the list of snippet pinned to the current context
+   * @params {Integer} start The start index of pinned documents
+   * @param cb(data) The callback containning the HTML generated
+   */
+  ret.getPinnedDocuments = function(start, cb) {
+    var baseUrl = '/app/pinned';
+    if (start) {
+      baseUrl += '?start=' + start;
+    }
+
+    var url = _setDataInUrl(baseUrl);
+    $.get(url, cb);
+  };
+
+  /**
+   * Pin a document to the current context
+   * @params {String} docId The document to pin
+   * @param cb(err, data) The callback containning the HTML generated
+   */
+  ret.pinDocument = function(docId, cb) {
+    if (!docId) {
+      return new Error('No document to pin');
+    }
+
+    var baseUrl = '/app/add-pin/' + docId;
+    var url = _setDataInUrl(baseUrl);
+
+    $.get(url, function() {
+      ret.getPinnedDocuments(0, cb);
     });
-  }
-});
+  };
 
-/**
- * Handle filters
- */
-$(function() {
-  $("#show-filters").click(function(e) {
-    e.preventDefault();
-    $("#about-string").addClass("hidden");
-    $("#filters-container").removeClass("hidden");
-  });
-
-  $("#hide-filters").click(function(e) {
-    e.preventDefault();
-    $("#about-string").removeClass("hidden");
-    $("#filters-container").addClass("hidden");
-  });
-
-  $("#send-filters").click(function(e) {
-    e.preventDefault();
-
-    var filters = {};
-
-    var provider = $('#provider').val();
-    if (provider.length > 0) {
-      filters.provider = provider;
+  /**
+   * Unpin a document of the current context
+   * @params {String} docId The document to unpin
+   * @param cb(err, data) The callback containning the HTML generated
+   */
+  ret.unpinDocument = function(docId, cb) {
+    if (!docId) {
+      return new Error('No document to unpin');
     }
 
-    var dT = $('#document_type').val();
-    if (dT.length > 0) {
-      filters.document_type = dT;
-    }
+    var baseUrl = '/app/remove-pin/' + docId;
+    var url = _setDataInUrl(baseUrl);
 
-    var url = '/app/context-search?filters=' + encodeURIComponent(JSON.stringify(filters));
-
-    goToLocation(window, url);
-  });
-});
-
-/**
- * Access pages
- */
-$(function() {
-  $(".link").click(function(e) {
-    e.preventDefault();
-    if ($(this).hasClass('link-popup')) {
-      attachedViewer = window.open(null,"_blank","scrollbars=yes, resizable=yes, width=600, height=800");
-      goToLocation(attachedViewer, e.target.href);
-    } else {
-      goToLocation(window, e.target.href);
-    }
-  });
-});
-
-/**
- * Infinite scroll
- */
-$(function() {
-  if($('#loading-documents')) {
-    var loading = false;
-    var currentCount = 20;
-    var lastDocument = false;
-
-    $(window).scroll(function(){
-      if (!loading && !lastDocument && $(window).scrollTop() === $(document).height() - $(window).height()){
-
-        loading = true;
-        $('#loading-documents').removeClass('hidden');
-        var loadingUrl = document.URL + '&start=' + currentCount;
-
-        $.get(loadingUrl, function(res) {
-          if (!res.length) {
-            lastDocument = true;
-          }
-
-          $(res).insertBefore('#loading-documents');
-          currentCount = $('.snippet-container').length;
-          $('#loading-documents').addClass('hidden');
-          loading = false;
-        });
-      }
+    $.get(url, function() {
+      ret.getPinnedDocuments(0, cb);
     });
-  }
-});
+  };
+
+  /**
+   * Go to the curent Context
+   * @params {String} url URL to visit
+   */
+  ret.goTo = function(url) {
+    window.location(_setDataInUrl(url));
+  };
+
+  return ret;
+})();

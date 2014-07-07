d5a192dcfd80335f15c15446188a30dfd577c9bb
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-07-07 17:17:16 +0200

    mobile navigation + doc list

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,15 @@
-function salesfetchViewModel() {
+
+var docTotalNumber = 0;
+
+function Document(name, provider, isStarred) {
+    var self = this;
+    self.name = name;
+    self.provider = provider;
+    self.isStarred = isStarred;
+    self.id = docTotalNumber++;
+}
+
+function SalesfetchViewModel() {
 
     var client = this;
     client.leftTabs = ['Timeline', 'Stared'];
@@ -7,26 +18,41 @@ function salesfetchViewModel() {
     client.chosenTabData = ko.observable();
     client.chosenDocumentData = ko.observable();
 
+    // Editable data
+    client.documents = ko.observableArray([
+        new Document("PDF", "Dropbox", false),
+        new Document("Note", "Evernote", true),
+        new Document("Picture", "Dropbox", false),
+        new Document("Spreadsheet", "Drive", true),
+        new Document("Mail", "Gmail", false)
+    ]);
+
     // Behaviours
-    client.goToTab = function(tab) { location.hash = tab };
-    client.goToDocument = function(document) { location.hash = document.tab + '/' + document.id };
+    client.goToTab = function(tab) {
+        // location.hash = tab
+        client.chosenTabId(tab);
+        client.chosenDocumentData(null);
+        client.chosenTabData(client.documents());
+        console.log(client.chosenTabData());
+    };
+    client.goToDocument = function(document) {
+        // location.hash = document.tab + '/' + document.id
+        client.chosenTabData(null);
+        client.chosenDocumentData(document);
+    };
 
-    // Client-side routes
-    Sammy(function() {
-        this.get('#:tab', function() {
-            client.chosenTabId(this.params.tab);
-            client.chosenDocumentData(null);
-            client.chosenTabData({id: "Timeline", documents: [{id: 1, name: "Dopbox", tab: 'Timeline'}, {id: 2, name: "Mail", tab: 'Stared'}]});
-        });
+    client.goBack = function() {
+        console.log('coucou');
+        client.goToTab('Timeline');
+    }
 
-        this.get('#:tab/:documentId', function() {
-            client.chosenTabId(this.params.tab);
-            client.chosenTabData(null);
-            client.chosenDocumentData({id:'Stared', name: 'myDocument'});
-            // $.get("./document", { documentId: this.params.documentId }, client.chosenDocumentData);
-        });
+    client.setDocuments = function(documents) {
+        client.chosenTabData(documents);
+    };
 
-    }).run();
+
+    // Show Timeline by default
+    client.goToTab('Timeline');
 };
 
-ko.applyBindings(new salesfetchViewModel());
+ko.applyBindings(new SalesfetchViewModel());

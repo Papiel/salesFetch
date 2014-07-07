eadb59ac622f08084e27776c80812552fc24a953
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-07-07 17:38:41 +0200

    providers and types

--- public/js/client.js
+++ public/js/client.js
@@ -1,12 +1,13 @@
 
 var docTotalNumber = 0;
 
-function Document(name, provider, isStarred) {
+function Document(name, type, provider, isStarred) {
     var self = this;
     self.name = name;
     self.provider = provider;
     self.isStarred = isStarred;
     self.id = docTotalNumber++;
+    self.type = type;
 }
 
 function SalesfetchViewModel() {
@@ -19,21 +20,27 @@ function SalesfetchViewModel() {
     client.chosenDocumentData = ko.observable();
 
     // Editable data
-    client.documents = ko.observableArray([
-        new Document("PDF", "Dropbox", false),
-        new Document("Note", "Evernote", true),
-        new Document("Picture", "Dropbox", false),
-        new Document("Spreadsheet", "Drive", true),
-        new Document("Mail", "Gmail", false)
-    ]);
+    client.documents = ko.observableArray([]);
+    client.providers = ko.observableArray([]);
+    client.types = ko.observableArray([]);
 
+    client.addDocument = function(document) {
+        client.documents.push(document);
+        client.providers.push(document.provider);
+        client.types.push(document.type);
+    };
+
+    client.addDocuments = function(documents) {
+        documents.forEach(function(document) {
+            client.addDocument(document);
+        });
+    }
     // Behaviours
     client.goToTab = function(tab) {
         // location.hash = tab
         client.chosenTabId(tab);
         client.chosenDocumentData(null);
         client.chosenTabData(client.documents());
-        console.log(client.chosenTabData());
     };
     client.goToDocument = function(document) {
         // location.hash = document.tab + '/' + document.id
@@ -42,7 +49,6 @@ function SalesfetchViewModel() {
     };
 
     client.goBack = function() {
-        console.log('coucou');
         client.goToTab('Timeline');
     }
 
@@ -53,6 +59,18 @@ function SalesfetchViewModel() {
 
     // Show Timeline by default
     client.goToTab('Timeline');
+
+
+
+    // Demo
+
+    client.addDocuments([
+        new Document("Contrat 12", "PDF", "Dropbox", false),
+        new Document("Oublie pas !", "Note", "Evernote", true),
+        new Document("Vacance 117.jpg", "Picture", "Dropbox", false),
+        new Document("Facture", "PDF", "Drive", true),
+        new Document("FWD: #laMamanDeRicard", "Mail", "Gmail", false)
+    ]);
 };
 
 ko.applyBindings(new SalesfetchViewModel());

f4ef9cd995ef5717d248da0355dfe2b7632ec612
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-09 16:44:02 +0200

    Should mark pinned documents as pinned

--- test/controllers/app/documents/index.js
+++ test/controllers/app/documents/index.js
@@ -3,16 +3,26 @@
 var should = require('should');
 var request = require('supertest');
 var async = require('async');
+var rarity = require('rarity');
+
+var mongoose = require('mongoose');
+var Pin = mongoose.model('Pin');
 
 var app = require('../../../../app.js');
 var cleaner = require('../../../hooks/cleaner');
 var requestBuilder = require('../../../helpers/login').requestBuilder;
+var getUser = require('../../../helpers/login').getUser;
 var APIs = require('../../../helpers/APIs');
 var checkUnauthenticated = require('../../../helpers/access').checkUnauthenticated;
 
-
 describe('/app/documents page', function() {
   var endpoint = '/app/documents';
+  var context = {
+    recordType: 'Contact',
+    recordId: '003b000000LHOj3',
+    templatedQuery: 'Walter White',
+    templatedDisplay: 'Walter White'
+  };
 
   beforeEach(cleaner);
   beforeEach(function(done) {
@@ -22,14 +32,7 @@ describe('/app/documents page', function() {
   describe('GET /app/documents', function() {
     checkUnauthenticated(app, 'get', endpoint);
 
-    it("should return contextual data", function(done) {
-      var context = {
-        recordType: 'Contact',
-        recordId: '003b000000LHOj3',
-        templatedQuery: 'Walter White',
-        templatedDisplay: 'Walter White'
-      };
-
+    it("should return a list of documents", function(done) {
       async.waterfall([
         function buildRequest(cb) {
           requestBuilder(endpoint, context, null, cb);
@@ -49,19 +52,11 @@ describe('/app/documents page', function() {
     });
 
     it("should return snippets for infinite scroll if there is a start query parameter", function(done) {
-
-      var context = {
-        recordType: 'Contact',
-        recordId: '003b000000LHOj3',
-        templatedQuery: 'Walter White',
-        templatedDisplay: 'Walter White'
-      };
-
-      endpoint += '?start=1';
+      var endpointWithStart = endpoint + '?start=1';
 
       async.waterfall([
         function buildRequest(cb) {
-          requestBuilder(endpoint, context, null, cb);
+          requestBuilder(endpointWithStart, context, null, cb);
         },
         function sendRequest(url, cb) {
           request(app)
@@ -73,6 +68,53 @@ describe('/app/documents page', function() {
               res.text.should.containDeep("National Security");
             })
             .end(cb);
+        }
+      ], done);
+    });
+
+    it("should mark pinned documents as pinned", function(done) {
+      var url;
+      var pinnedId = '5320a773bc2e51d7135f0c8f';
+
+      async.waterfall([
+        function buildRequest(cb) {
+          requestBuilder(endpoint, context, null, cb);
+        },
+        function getTestUser(theUrl, cb) {
+          url = theUrl;
+          getUser(cb);
+        },
+        function addPin(user, cb) {
+          var pin = new Pin({
+            createdBy: user.id,
+            SFDCId: context.recordId,
+            anyFetchId: pinnedId
+          });
+
+          pin.save(rarity.slice(1, cb));
+        },
+        function sendRequest(cb) {
+          should(url).be.ok;
+          request(app)
+            .get(url)
+            .expect(200)
+            .end(cb);
+        },
+        function checkPinnedDocument(res, cb) {
+          should(res.body).be.ok;
+          should(res.body.documents).be.ok;
+          should(res.body.documents.data).be.ok;
+
+          var docs = res.body.documents.data;
+          var pinnedDoc;
+          docs.forEach(function(doc) {
+            if(doc.id === pinnedId) {
+              pinnedDoc = doc;
+            }
+          });
+          should(pinnedDoc).be.ok;
+          pinnedDoc.should.have.properties({ pinned: true });
+          cb();
         }
       ], done);
     });

fe3f1cef68c2a0dbfcccdceef997f3ad05aae640
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-09 17:10:56 +0200

    Test for filters

--- test/controllers/app/documents/index.js
+++ test/controllers/app/documents/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var should = require('should');
 var request = require('supertest');
@@ -32,7 +32,7 @@ describe('/app/documents page', function() {
   describe('GET /app/documents', function() {
     checkUnauthenticated(app, 'get', endpoint);
 
-    it("should return a list of documents", function(done) {
+    it('should return a list of documents', function(done) {
       async.waterfall([
         function buildRequest(cb) {
           requestBuilder(endpoint, context, null, cb);
@@ -51,7 +51,7 @@ describe('/app/documents page', function() {
       ], done);
     });
 
-    it("should return snippets for infinite scroll if there is a start query parameter", function(done) {
+    it('should return snippets for infinite scroll if there is a start query parameter', function(done) {
       var endpointWithStart = endpoint + '?start=1';
 
       async.waterfall([
@@ -65,14 +65,14 @@ describe('/app/documents page', function() {
             .expect(function(res) {
               should(res.body).be.ok;
               res.body.should.have.properties('facets', 'data');
-              res.text.should.containDeep("National Security");
+              res.text.should.containDeep('National Security');
             })
             .end(cb);
         }
       ], done);
     });
 
-    it("should mark pinned documents as pinned", function(done) {
+    it('should mark pinned documents as pinned', function(done) {
       var url;
       var pinnedId = '5320a773bc2e51d7135f0c8f';
 
@@ -119,7 +119,41 @@ describe('/app/documents page', function() {
       ], done);
     });
 
-    it("should err if no template is found", function(done) {
+    it('should apply filters', function(done) {
+      var filters = {
+        provider: '5320a682c8318cba94000040'
+      };
+
+      async.waterfall([
+        function buildRequest(cb) {
+          requestBuilder(endpoint, context, null, cb);
+        },
+        function sendRequest(url, cb) {
+          request(app)
+            .get(url)
+            .query({ filters: filters })
+            .expect(200)
+            .end(cb);
+        },
+        function checkResponse(res, cb) {
+          should(res.body).be.ok;
+          res.body.should.have.keys('documents', 'filters');
+          res.body.documents.should.have.property('data');
+
+          var docs = res.body.documents.data;
+          docs.should.not.be.empty;
+          docs.forEach(function(doc) {
+            doc.should.have.property('provider');
+            doc.provider.should.have.properties({
+              name: 'Dropbox'
+            });
+          });
+          cb();
+        }
+      ], done);
+    });
+
+    it('should err if no template is found', function(done) {
       var context = {
         recordType: 'Contact',
         recordId: '003b000000LHOj3',

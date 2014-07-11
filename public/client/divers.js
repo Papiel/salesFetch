f7f305c096613751815faa7f91f6afb2ea87606e
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-07-11 16:01:54 +0200

    open/close main view with animation

--- public/client/divers.js
+++ public/client/divers.js
@@ -1,7 +1,29 @@
+/*===============
+  GLOBAL
+ ================*/
 
-// -------------- Override Bootstrap actions
-
-// Do not close filters menu when click inside
+/*
+ * Override Bootstrap actions
+ * Do not close filters menu when click inside
+ */
 $('#filter-tabs .dropdown-menu a').click(function(e) {
     e.stopPropagation();
+});
+
+/*===============
+  TABLET
+ ================*/
+
+/**
+ * Toogle left panel
+ */
+$('#toggle-panel-btn').click(function() {
+  $("#mainview").toggleClass('active');
+});
+
+/**
+ * Hide left bar on click snippet
+ */
+$('html.tablet.portrait .snippet').click(function(e) {
+  $("#mainview").removeClass('active');
 });

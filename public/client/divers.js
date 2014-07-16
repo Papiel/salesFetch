0ae354f0f229e8baf00788c4c9df3674a4770d31
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-07-16 15:54:32 +0200

    toggle panel button style

--- public/client/divers.js
+++ public/client/divers.js
@@ -19,6 +19,7 @@ $('#filter-tabs .dropdown-menu a').click(function(e) {
  */
 $('#toggle-panel-btn').click(function() {
   $("#mainview").toggleClass('active');
+  $("#toggle-panel-btn span").toggleClass('fa-toggle-left');
 });
 
 /**
@@ -26,4 +27,13 @@ $('#toggle-panel-btn').click(function() {
  */
 $('html.tablet.portrait .snippet').click(function(e) {
   $("#mainview").removeClass('active');
+  $("#toggle-panel-btn span").removeClass('fa-toggle-left');
+});
+
+/**
+ * Hide left bar on click subview
+ */
+$('html.tablet.portrait #subview').click(function(e) {
+  $("#mainview").removeClass('active');
+  $("#toggle-panel-btn span").removeClass('fa-toggle-left');
 });

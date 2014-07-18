6b51e28fb23e075901d9663826a33d32bc1e53f6
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-18 14:39:29 +0200

    Minifying with Gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 'use strict';
 
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
 var jshint = require('gulp-jshint');
 var less = require('gulp-less');
-var nodemon = require('gulp-nodemon');
+
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var minifyJs = require('gulp-uglify');
+
 
 var paths = {
   js: ['gruntfile.js', 'app.js', 'config/**/*.js', 'app/**/*.js', 'public/js/**', 'test/**/*.js'],
   less: {
+    watch: 'assets/less/**/*.less',
     source: 'assets/less/style.less',
     target: 'public/stylesheets'
+  },
+  minify: {
+    css: 'public/stylesheets/style.css',
+    js: 'public/js/**/*.js',
+    target: 'public/dist/'
   },
   ignores: ['/lib/**', 'public/**']
 };
@@ -24,6 +36,9 @@ var nodemonOptions = {
   ignore: paths.ignores
 };
 
+// TODO: compile fontawesome to use a few glyphs only?
+// TODO: compile all lib JS & CSS to a single HTML base skeleton file using an HTML template, then uglify the HTML
+
 // JS linting
 gulp.task('lint', function() {
   return gulp.src(paths.js)
@@ -32,30 +47,35 @@ gulp.task('lint', function() {
 });
 
 // LESS compiling
-// TODO: source maps?
 gulp.task('less', function() {
   return gulp.src(paths.less.source)
     .pipe(less())
     .pipe(gulp.dest(paths.less.target));
 });
 
-// TODO: add a task to minify CSS & JS
-// TODO: compile fontawesome to use a few glyphs only?
-// TODO: compile all lib JS & CSS to a single HTML base skeleton file using an HTML template
+gulp.task('minify', function() {
+  gulp.src(paths.minify.css)
+    .pipe(minifyCss())
+    .pipe(gulp.dest(paths.minify.target));
+
+  gulp.src(paths.minify.js)
+    .pipe(concat('all.js'))
+    .pipe(minifyJs())
+    .pipe(gulp.dest(paths.minify.target));
+});
 
 // Nodemon (auto-restart node-apps)
 gulp.task('nodemon', function() {
-  nodemon(nodemonOptions)
-    .on('change', ['lint']);
+  nodemon(nodemonOptions);
 });
-
 
 // Auto-run tasks on file changes
 gulp.task('watch', function() {
   gulp.watch(paths.js, ['lint']);
-  gulp.watch(paths.less.source, ['less']);
+  gulp.watch(paths.less.watch, ['less']);
 });
 
-gulp.task('default', ['lint', 'less', 'nodemon'], function() {
+// Run main tasks on launch
+gulp.task('default', ['lint', 'less', 'watch', 'nodemon'], function() {
   console.log('Hello gulp');
 });

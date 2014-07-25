6ddc11bdea75f5bd2aee961d975c7edb52421518
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-25 16:00:25 +0200

    Use URL encode to bypass UTF-8 representation problems w/ SalesForce

--- app/helpers/get-secure-hash.js
+++ app/helpers/get-secure-hash.js
@@ -6,6 +6,14 @@ var crypto = require('crypto');
 var config = require('../../config/configuration.js');
 
 /**
+ * SalesForce's URI encoding function strictly adheres to RFC 3986
+ */
+var strictUriEncode = function(str) {
+  // Note that `replace` can be called with a function (similar to `map`)
+  return encodeURIComponent(str).replace(/[!\'()\*]/g, escape);
+};
+
+/**
  * The hash is computed from params taken from the request and a secret key.
  * @param {Object} data The GET param describing the request (with `user`, `organization`, ...)
  * @param {String} masterKey The user's organization's master key
@@ -15,6 +23,7 @@ module.exports = function getSecureHash(data, masterKey) {
   var usefulData = _.merge({}, data);
   usefulData.hash = null;
 
-  var hash = JSON.stringify(usefulData) + masterKey + config.secretKey;
+  var encoded = strictUriEncode(JSON.stringify(usefulData));
+  var hash = encoded + masterKey + config.secretKey;
   return crypto.createHash('sha1').update(hash).digest("base64");
 };

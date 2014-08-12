57250f25cf71ffa8534a90e5e635171661822ac5
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-08-12 16:37:22 +0200

    lint

--- assets/js/helpers/string.js
+++ assets/js/helpers/string.js
@@ -6,10 +6,10 @@
  */
 String.prototype.format = function() {
   var s = this;
-  for (var i = 0; i < arguments.length; i++) {
+  for (var i = 0; i < arguments.length; i+=1) {
     var reg = new RegExp("\\{" + i + "\\}", "gm");
     s = s.replace(reg, arguments[i]);
   }
 
   return s;
-}
+};

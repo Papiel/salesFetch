9229fe1799795a9603620c2da65634ae49471f92
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-08-20 18:04:54 +0200

    pullRight explained

--- assets/js/models/DocumentTab.js
+++ assets/js/models/DocumentTab.js
@@ -9,7 +9,7 @@ var noopFilter = function() {
 /**
  * @param {String} name
  * @param {String} display The font-awesome CSS class corresponding to the icon of this tab
- * @param {Boolean} pullRight
+ * @param {Boolean} pullRight If true the tab will align right in horizontal menu
  * @param {Function} filter A function to use to filter the documents to be shown in this tab
  */
 module.exports = function DocumentTab(client, name, display, pullRight, starred, filter) {

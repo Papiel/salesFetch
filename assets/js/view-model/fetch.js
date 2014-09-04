fd62cd7470a9c540e43f47cf11657c25cf59767f
Neamar <neamar@neamar.fr>
2014-09-04 19:15:40 +0200

    Display spinner

--- assets/js/view-model/fetch.js
+++ assets/js/view-model/fetch.js
@@ -52,6 +52,8 @@ module.exports.fetchDocuments = function(updateFacets) {
     tab.shouldDisplayDocumentsSpinner(false);
     tab.documentListError(getErrorMessage(res));
     tab.allDocumentsLoaded(true);
+    tab.client.hasFinishedLoading(true);
+
   });
 };
 

f458db95d5d2c5b439268aa415637a69d3de0443
bolvy_a <bolvy_a@epitech.eu>
2014-09-24 13:05:10 +0200

    Encoding

--- assets/js/view-model/navigation.js
+++ assets/js/view-model/navigation.js
@@ -74,6 +74,8 @@ module.exports.goToDocument = function(doc) {
       // this allows links without host to work
       $(target.head).append('<base href="https://' + document.location.host + '" />');
 
+      $(target.head).append('<meta charset="utf-8">');
+
       $(target.head).append('<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">');
       $(target.head).append('<link rel="stylesheet" href="/dist/full-view.css" type="text/css">');
       $(target.head).append('<link rel="stylesheet" href="/dist/index.min.css" type="text/css">');

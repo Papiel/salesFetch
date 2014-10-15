89320fbdaf097b89e086fdfa56c061bbcbb44499
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-10-15 13:45:01 +0200

    Return custom message instead of create user automatically

--- app/middlewares/authorization.js
+++ app/middlewares/authorization.js
@@ -3,6 +3,7 @@
 var restify = require('restify');
 var async = require('async');
 var rarity = require('rarity');
+var url = require('url');
 
 var mongoose =require('mongoose');
 var Organization = mongoose.model('Organization');
@@ -16,20 +17,27 @@ var getSecureHash = require('../helpers/get-secure-hash.js');
  * Authenticate the user based on the request's context
  * @return {Object} the user
  */
-var authenticateUser = function(context, org, done) {
+var authenticateUser = function(context, org, req, done) {
   var userContext = context.user;
   async.waterfall([
     function(cb) {
       // Find an existing user
       User.findOne({SFDCId: userContext.id}, cb);
-    }, function(user, cb) {
+    },
+    function(user, cb) {
       if(user) {
-        return done(null, user);
+        return cb(null, user);
       }
 
       // If the user didn't exist in this company,
-      // create an AnyFetch account in his name
-      anyFetchHelpers.addNewUser(userContext, org, cb);
+      // create an AnyFetch account in his name if we handle the init request
+      var parsed = url.parse(req.url);
+      var path = parsed.pathname;
+      if(path && path.indexOf('/app/init') !== -1) {
+        return anyFetchHelpers.addNewUser(userContext, org, cb);
+      }
+      // or, we'll return custom message to redirect the user to the init page
+      cb(new restify.InvalidCredentialsError('User not created'));
     }
   ], done);
 };
@@ -78,7 +86,7 @@ module.exports.requiresLogin = function(req, res, next) {
       cb(null, data);
     },
     function loadUser(envelope, cb) {
-      authenticateUser(envelope, organization, rarity.slice(2, cb));
+      authenticateUser(envelope, organization, req, rarity.slice(2, cb));
     },
     function updateCompanyIfNecessary(user, cb) {
       // If no one in the company had logged-in for a while

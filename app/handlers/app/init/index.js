129b20194c456bef5dd4e7eb9d651b2bd946715f
Neamar <neamar@neamar.fr>
2014-12-12 16:10:22 +0100

    fix last comments

--- app/handlers/app/init/index.js
+++ app/handlers/app/init/index.js
@@ -4,24 +4,16 @@ var async = require('async');
 var rarity = require('rarity');
 var restify = require('restify');
 
-var mongoose = require('mongoose');
-var Organization = mongoose.model('Organization');
-
 var anyfetchHelpers = require('../../../helpers/anyfetch.js');
 
-// create an account in the name of the user if he doesn't exist
+
+// Create an account in the name of the user if he doesn't exist
+// Return no data
+// This handler is protected by ensureValidHash (elsewise anyone would be able to add a user on any organization)
 module.exports.post = function createAccount(req, res, next) {
   async.waterfall([
-    function getOrg(cb) {
-      // Find an existing company
-      Organization.findOne({SFDCId: req.data.organization.id}, cb);
-    },
-    function addNewUser(org, cb) {
-      if(!org) {
-        return cb(new restify.InvalidCredentialsError('No company matching this id has been found'));
-      }
-
-      anyfetchHelpers.addNewUser(req.data.user, org, rarity.slice(2, cb));
+    function addNewUser(cb) {
+      anyfetchHelpers.addNewUser(req.data.user, req.organization, rarity.slice(2, cb));
     },
     function sendData(user, cb) {
       res.send(204);

0fa5a73801da05c64e464510a4ac440b1ebc763c
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-12-12 12:01:15 +0100

    Restrict ObjectId check

--- app/routes.js
+++ app/routes.js
@@ -21,9 +21,9 @@ module.exports = function(server) {
     handlers.app.documents.index.get);
 
   server.get('/app/documents/:id',
+    middlewares.idIsObjectId,
     middlewares.checkParams,
     middlewares.authorization,
-    middlewares.idIsObjectId,
     middlewares.requiresContext,
     handlers.app.documents.id.index.get);
 
@@ -34,16 +34,16 @@ module.exports = function(server) {
     handlers.app.pins.index.get);
 
   server.post('/app/pins/:id',
+    middlewares.idIsObjectId,
     middlewares.checkParams,
     middlewares.authorization,
-    middlewares.idIsObjectId,
     middlewares.requiresContext,
     handlers.app.pins.id.index.post);
 
   server.del('/app/pins/:id',
+    middlewares.idIsObjectId,
     middlewares.checkParams,
     middlewares.authorization,
-    middlewares.idIsObjectId,
     middlewares.requiresContext,
     handlers.app.pins.id.index.del);
 
@@ -53,6 +53,7 @@ module.exports = function(server) {
     handlers.app.providers.index.get);
 
   server.post('/app/providers/:id',
+    middlewares.idIsObjectId,
     middlewares.checkParams,
     middlewares.authorization,
     handlers.app.providers.index.post);

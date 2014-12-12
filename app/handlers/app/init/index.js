b26144d14797803f28cb4db1ec8e299dc6216ac6
buathi_q <buathi_q@epitech.eu>
2014-12-12 14:31:50 +0100

    Handle creating of mongo duplicate key

--- app/handlers/app/init/index.js
+++ app/handlers/app/init/index.js
@@ -5,7 +5,6 @@ var rarity = require('rarity');
 var restify = require('restify');
 
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
 var Organization = mongoose.model('Organization');
 
 var anyfetchHelpers = require('../../../helpers/anyfetch.js');
@@ -13,26 +12,27 @@ var anyfetchHelpers = require('../../../helpers/anyfetch.js');
 // create an account in the name of the user if he doesn't exist
 module.exports.post = function createAccount(req, res, next) {
   async.waterfall([
-    function getUser(cb) {
-      // Find an existing user
-      User.findOne({SFDCId: req.data.user.id}, cb);
-    },
-    function getOrg(user, cb) {
+    function getOrg(cb) {
       // Find an existing company
-      Organization.findOne({SFDCId: req.data.organization.id}, rarity.carry([user], cb));
+      Organization.findOne({SFDCId: req.data.organization.id}, cb);
     },
-    function addNewUser(user, org, cb) {
+    function addNewUser(org, cb) {
       if(!org) {
         return cb(new restify.InvalidCredentialsError('No company matching this id has been found'));
       }
-      if(user) {
-        return cb(null, user);
-      }
+
       anyfetchHelpers.addNewUser(req.data.user, org, rarity.slice(2, cb));
     },
     function sendData(user, cb) {
       res.send(204);
       cb();
     }
-  ], next);
+  ], function(err) {
+    if(err && err.toString().match(/duplicate key error/i)) {
+      err = null;
+      res.send(204);
+    }
+
+    return next(err);
+  });
 };

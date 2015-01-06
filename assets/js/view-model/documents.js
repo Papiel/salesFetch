52612d60034b5b67d982b950ef924bea2a29c687
Neamar <neamar@neamar.fr>
2015-01-06 14:33:33 +0100

    Renamed Document to DocumentModel

--- assets/js/view-model/documents.js
+++ assets/js/view-model/documents.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var DocumentModel = require('../models/Document.js');
 var Type = require('../models/Type.js');
 var Provider = require('../models/Provider.js');
 
@@ -20,7 +21,7 @@ module.exports.documentWithJson = function(json) {
   });
   // Or create it if it does not exist yet
   if(!doc) {
-    doc = new require('../models/Document.js')(json, tab.client.starredTab);
+    doc = new DocumentModel(json, tab.client.starredTab);
   }
 
   // Instantiate a new Provider model only when needed

6816686f6d6948232ef2038a69d1c50e8b43a54c
Amoki <hugo.duroux@gmail.com>
2015-02-17 16:30:00 +0100

    update opbeat variable names

--- config/configuration.js
+++ config/configuration.js
@@ -17,9 +17,9 @@ var errorsPath = rootPath + '/app/views/errors';
 // Third party services
 var services = {};
 services.opbeat = {
-  organizationId: process.env.OPBEAT_ORG_ID,
+  organizationId: process.env.OPBEAT_ORGANIZATION_ID,
   appId: process.env.OPBEAT_APP_ID,
-  secretToken: process.env.OPBEAT_TOKEN,
+  secretToken: process.env.OPBEAT_SECRET_TOKEN,
 };
 
 var baseConfig = {
